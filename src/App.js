675b457905f8cbb47cb1ab6954fbd69d5dcdb55a
Michael Zaleski <me@mikezaleski.com>
2020-10-02 15:37:13 -0400

    Add Global Styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import GlobalStyle from './globalStyles';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Router>
         <Switch>
-          <Route path='/search'>Search Route</Route>
-          <Route path='/'>Root Route</Route>
+          <Route path="/search">Search Route</Route>
+          <Route path="/">Root Route</Route>
         </Switch>
       </Router>
       <Footer />
---
+++ src/globalStyles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #ffffff;
+  }
+`;
+
+export default GlobalStyle;
