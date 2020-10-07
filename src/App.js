c94ae50ff6811626c1b82f87dce736284d397be0
Michael Zaleski <me@mikezaleski.com>
2020-10-07 14:44:23 -0400

    Move ThemeProvider to App.js
    Relocated styled-components <THemeProvider> import from index.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './globalStyles';
+
 import Header from './components/Header';
 import Search from './pages/Search';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import theme from './config/theme';
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <Header />
-      <Router>
-        <Switch>
-          <Route path="/search">
-            <Search />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Header />
+        <Router>
+          <Switch>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+        <Footer />
+      </ThemeProvider>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
 
 import App from './App';
-import theme from './config/theme';
-
 import 'normalize.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
