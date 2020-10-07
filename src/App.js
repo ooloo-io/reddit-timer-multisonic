247ed9960bd3a50a0e1ab87be630d0721f166a3f
Mike Zaleski <me@mikezaleski.com>
2020-10-07 19:41:11 -0400

    Create app skeleton (#1)
    * Install fonts (Bitter and Montserrat)
    
    Installed fonts Bitter and Montserrat in the head of public/index.html
    
    * Reset Default Browser Styles
    
    Reset default browser styles using normalize.css
    
    * Add Basic Routing.  Add placholder Header and Footer components.
    
    * Add Global Styles
    
    * Update Project Structure
    
    Added folders for `/api/` and `/pages` and moved `Search.js` and `Home.js` into `/pages/`.
    Import `Home` and `Search` components into `App.js` to display placheholder text.
    
    * Update Font Imports, Global Styles and Theme
    
    Changed font imports to local WOFF and WOFF2 files
    Updated GlobalStyles with new font imports, plus body values based on new Theme.js
    Added Theme.js to provide default colors, fonts, font weight, and line height
    
    * Remove exact attribute from Search Route
    
    * Revert "Remove exact attribute from Search Route"
    
    This reverts commit 960a550376333628e4de33045ff7343548ccd0a8.
    
    * Remove exact attribute from Search Route
    
    Also add trailing comma in index.js to pass lint.
    
    * Remove unused import (Styled) from App.js
    
    * Update App Title in index.html
    
    * Move ThemeProvider to App.js
    
    Relocated styled-components <THemeProvider> import from index.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './globalStyles';
+
+import Header from './components/Header';
+import Search from './pages/Search';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import theme from './config/theme';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Header />
+        <Router>
+          <Switch>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+        <Footer />
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Footer() {
+  return <div>Footer placeholder</div>;
+}
+
+export default Footer;
