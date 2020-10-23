c7c4bb477d0b9d7fd7c02fb69189ee7d739fde45
Michael Zaleski <me@mikezaleski.com>
2020-10-23 15:14:25 -0400

    Update Header.style.js and globalStyle with suggested implementation details

--- src/common/header/Header.style.js
+++ src/common/header/Header.style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ReactComponent as UnstyledLogo } from './logo.svg';
 
-export const HeaderWrapper = styled.header`
+/* export const HeaderWrapper = styled.header`
   display: flex;
   height: 100px;
   padding: 0px 80px;
@@ -19,4 +19,25 @@ export const StyledLink = styled(Link)`
   color: ${(props) => props.theme.colors.grayBase};
   margin-left: 26px;
   text-decoration: none;
+`;
+ */
+
+export const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 100px;
+  margin: 0 auto;
+  padding: 0 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Logo = styled(UnstyledLogo)`
+  display: block;
+`;
+
+export const StyledLink = styled(Link)`
+  margin-left: 26px;
+  text-decoration: none;
+  color: ${(props) => props.theme.colors.grayBase};
 `;
